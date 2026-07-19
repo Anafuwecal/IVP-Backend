b9353922a0c423993e3b15441ff664ad65436ab4
Anafuwecal <anafuwecalebifeanyi@gmail.com>
2026-07-19 16:36:09 +0100

    test: verify CI/CD pipeline execution

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
-
+// Pipeline verification test
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
