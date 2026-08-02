b853eefb502851ee0cdd77b1130c3ccaabb1d9ed
Anafuwecal <anafuwecalebifeanyi@gmail.com>
2026-08-02 16:40:19 +0100

    Health statement

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    return {
+      message: 'Welcome to the IVP Africa API',
+      status: 'online',
+      version: '1.0.0'
+    };
   }
 }
